1dc0dcc551c8e54c7e26f819957293994912c240
Alex Schwartz <aschwartz2018@fau.edu>
2021-08-31 11:03:09 -0400

    Fix permission issue

--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alex/Desktop/django/myvenv"
+set -gx VIRTUAL_ENV "/home/alex/Desktop/DjangoTutorial/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- myvenv/bin/django-admin.py
+++ myvenv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/alex/Desktop/django/myvenv/bin/python
+#!/home/alex/Desktop/DjangoTutorial/myvenv/bin/python
 from django.core import management
 
 if __name__ == "__main__":
